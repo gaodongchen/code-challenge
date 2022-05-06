52c7045931c6e54a3b77a01fe2061a1549f30651
高东臣 <gaodongchen@qq.com>
2022-05-06 19:57:05 +0800

    Update index view

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,41 @@
 <?php
 
+use Codeception\PHPUnit\ResultPrinter\HTML;
+use yii\grid\GridView;
+use yii\helpers\Url;
+
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = Yii::$app->name . ' - 肥臣';
+
+echo GridView::widget([
+    'filterModel' => $searchModel,
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        [
+            'class' => yii\grid\CheckboxColumn::class,
+            'checkboxOptions' => function($model, $key, $index, $column){
+                return ['value' => $key];
+            }
+        ],
+        'id' => [
+            'attribute' => 'id',
+            'value' => function($model){
+                return $model->id;
+            }
+        ],
+        'name',
+        'code',
+        't_status' => [
+            'attribute' => 't_status',
+            'filter' => ['ok' => 'ok', 'hold' => 'hold'],
+        ],
+        [
+            'class' => 'yii\grid\ActionColumn',
+        ]
+    ],
+]);
 ?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+<div>
+    <a href="<?php echo Url::toRoute(array_merge(['site/export'], Yii::$app->request->get())); ?>">Export CSV</a>
 </div>
